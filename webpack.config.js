6aa688f15d309d0026d79f1c5f964c5112e4bd84
Adrian Hall <muzioclavecin@gmail.com>
2021-02-11 18:58:34 +0100

    config now supports css & template html

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "bundle.js"
+    },
+    devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({template: "./src/index.html"})
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/env"]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    }
+}
