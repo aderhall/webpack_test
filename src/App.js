a761516961adbf6a38c12c55bfdb31deef6156a8
Adrian Hall <muzioclavecin@gmail.com>
2021-02-12 00:10:48 +0100

    Working profile pages and current user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Reactive, useEffect, useState, apply } from "./reactive/reactive";
+import { Reactive, useEffect, useState, apply, set, deref } from "./reactive/reactive";
 
 function shuffleArray(array) {
     var currentIndex = array.length, temporaryValue, randomIndex, newArray = [...array];
@@ -27,15 +27,33 @@ function getData(url, callback) {
     xhr.send();
 }
 
+const staticStore = {
+    setPage: () => {},
+    setRoute: () => {},
+    myId: 3
+}
+
 function AppBar(props) {
     return Reactive.createElement(
         "div",
         { className: "AppBar" },
         Reactive.createElement(
-            "span",
+            "h1",
             { className: "AppBar__title" },
             props.name
-        )
+        ),
+        apply(links => {
+            return links.map(link => 
+                Reactive.createElement(
+                "a",
+                {
+                    className: "AppBar__link",
+                    href: "#",
+                    onClick: link[1]
+                },
+                link[0]
+            ))
+        }, [props.links])
     )
 }
 
@@ -68,9 +86,13 @@ function UserIcon(props) {
             }
         ),
         Reactive.createElement(
-            "span",
-            { className: "UserIcon__username" },
-            username
+            "a",
+            {
+                className: "UserIcon__username",
+                href: "#",
+                onClick: () => (staticStore.setRoute(deref(props.userId)), staticStore.setPage("profile"), console.log(props.userId))
+            },
+            apply(username => "@" + username, [username])
         )
     )
 }
@@ -149,22 +171,151 @@ function Feed(props) {
     )
 }
 
+function Profile(props) {
+    const [userInfo, setUserInfo] = useState(null);
+    const [imgUrl, setImgUrl] = useState(null);
+    
+    useEffect(userId => {
+        getData("https://jsonplaceholder.typicode.com/users/" + userId.toString(), xhr => {
+            if (xhr.status === 200) {
+                setUserInfo(JSON.parse(xhr.response));
+            } else {
+                setUserInfo(false);
+            }
+        });
+        getData("https://jsonplaceholder.typicode.com/photos/" + userId.toString(), xhr => {
+            if (xhr.status === 200) {
+                setImgUrl(JSON.parse(xhr.response).thumbnailUrl);
+            }
+        })
+    }, [props.userId]);
+    
+    return Reactive.createElement(
+        "div",
+        { className: "Profile__container" },
+        apply(userInfo => {
+            if (userInfo === null) {
+                return Reactive.createElement(
+                    "span",
+                    { className: "Profile__network-message" },
+                    "Getting profile info..."
+                );
+            } else if (userInfo === false) {
+                return Reactive.createElement(
+                    "span",
+                    { className: "Profile__network-message" },
+                    "Error: profile data not found"
+                )
+            } else {
+                return Reactive.createElement(
+                    "div",
+                    { className: "Profile" },
+                    Reactive.createElement(
+                        "div",
+                        { className: "Profile__head" },
+                        Reactive.createElement(
+                            "img",
+                            {
+                                className: "Profile__image",
+                                src: apply(imgUrl => imgUrl ? imgUrl : "", [imgUrl])
+                            },
+                        ),
+                        Reactive.createElement(
+                            "div",
+                            { className: "Profile__names" },
+                            Reactive.createElement(
+                                "span",
+                                { className: "Profile__name" },
+                                apply(userInfo => userInfo.name, [userInfo])
+                            ),
+                            Reactive.createElement(
+                                "span",
+                                { className: "Profile__username" },
+                                apply(userInfo => "@" + userInfo.username, [userInfo])
+                            )
+                        )
+                    ),
+                    Reactive.createElement(
+                        "dl",
+                        { className: "Profile__info" },
+                        Reactive.createElement(
+                            "dt",
+                            null,
+                            "Email:"
+                        ),
+                        Reactive.createElement(
+                            "dd",
+                            null,
+                            apply(userInfo => Reactive.createElement(
+                                "a",
+                                { href: "mailto:" + userInfo.email },
+                                userInfo.email
+                            ), [userInfo])
+                        ),
+                        Reactive.createElement(
+                            "dt",
+                            null,
+                            "Phone:"
+                        ),
+                        Reactive.createElement(
+                            "dd",
+                            null,
+                            apply(userInfo => Reactive.createElement(
+                                "a",
+                                { href: "tel:" + userInfo.phone },
+                                userInfo.phone
+                            ), [userInfo])
+                        )
+                    )
+                )
+            }
+        }, [userInfo])
+    )
+}
+
 function App(props) {
-    const appName = "KillerApp"
+    const appName = "Social Network";
+    
+    const [page, setPage] = useState("feed");
+    const [route, setRoute] = useState(null);
+    staticStore.setPage = setPage;
+    staticStore.setRoute = setRoute;
+
     return Reactive.createElement(
         "div",
         { className: "App" },
         Reactive.createElement(
             AppBar,
-            { name: appName }
+            {
+                name: appName,
+                links: [
+                    ["Feed", () => setPage("feed")],
+                    ["My Profile", () => setPage("my-profile")]
+                ]
+            }
         ),
         Reactive.createElement(
             "main",
             null,
-            Reactive.createElement(
-                Feed,
-                { url: "https://jsonplaceholder.typicode.com/posts"}
-            )
+            apply(page => {
+                if (page === "feed") {
+                    return Reactive.createElement(
+                        Feed,
+                        { url: "https://jsonplaceholder.typicode.com/posts"}
+                    );
+                } else if (page === "my-profile") {
+                    return Reactive.createElement(
+                        Profile,
+                        { userId: staticStore.myId }
+                    );
+                } else if (page === "profile") {
+                    return apply(route => Reactive.createElement(
+                        Profile,
+                        { userId: route }
+                    ), [route]);
+                }
+                
+            }, [page])
         )
     )
 }
